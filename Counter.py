835f2c2fe094fe85c750d116a076cf86b5351aef
cgonz330 <132164280+cgonz330@users.noreply.github.com>
2023-05-06 17:22:07 -0400

    Add files via upload

--- Counter.py
+++ Counter.py
@@ -1,5 +1,6 @@
 from Displays import *
 from Button import *
+from Buzzer import *
 import time
 """
 This is a counter class that will implement a basic
@@ -20,10 +21,13 @@ class Counter:
         
         self._greenButton = Button(17, "increase", buttonhandler=self, lowActive=True)
         self._redButton = Button(16, "reset", buttonhandler=self, lowActive=True)
-
+        self._buzz = PassiveBuzzer(13)
+        self._buzz.setVolume(10)
+        
     def increment(self):
         print("Counter: incrementing")
         self._number = self._number + 1
+        self.buzz()
 
     def reset(self):
         print("Counter: resetting")
@@ -31,7 +35,7 @@ class Counter:
         self._display.reset()
 
     def buttonPressed(self, name):
-        if name == "increase":
+        if name == 'increase':
             self.increment()
         elif name == "reset":
             self.reset()
@@ -43,9 +47,8 @@ class Counter:
         while True:
             self._display.showNumber(self._number)
             time.sleep(0.1)
-
-
-    
-
-
-
+            
+            
+    def buzz(self):
+        if self._number == 10:
+            self._buzz.beep()
