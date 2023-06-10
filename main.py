8fa9afd5eb9d29d57621c51a56bb92169e78aa05
cgonz330 <132164280+cgonz330@users.noreply.github.com>
2023-06-09 22:03:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-from TrafficLight import *
+from traffic_system import *
 
 # Create an instance of the TrafficLight class
-traffic_light = TrafficLight()
-
-# Start a new thread to display the traffic light state on the LCD
-def run_traffic_light(traffic_light):
-    traffic_light.cycle()
-
-run_traffic_light(traffic_light)
-
-# Start the main loop
-while True:
-    pass
+traffic_light = TrafficLightController()
+traffic_light.run()
